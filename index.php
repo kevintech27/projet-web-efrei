2683dcf58a16341e328601a27c42de235dfed07d
Alexandre34420 <landryalexandre868@gmail.com>
2026-03-20 15:27:49 +0100

    modif index accueil

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Département Informatique - EFREI Paris</title>
+</head>
+
+<body>
+
+    <header>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP_nJaWZpSKxzQ9oQWMMFpz-niIxdgt5xeww&s" alt="Logo EFREI Paris" width="120">
+        <h1>Former les ingénieurs et experts dans le futur du numérique</h1>
+
+        <nav>
+            <ul>
+                <li><a href="pageaccueil.html">Accueil</a></li>
+                <li><a href="cours-formations.html">Cours et Formations</a></li>
+                <li><a href="equipe-enseignement.html">Équipe d'enseignement</a></li>
+                <li><a href="partenariats-entreprises.html">Partenariats et entreprises</a></li>
+                <li><a href="vie-etudiante.html">Vie étudiante</a></li>
+                <li><a href="apropos.html">À propos</a></li>
+            </ul>
+        </nav>
+    </header>
