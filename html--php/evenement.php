3c735c0886338cbe0df27e33a4f32ed18baf9163
Alexandre34420 <landryalexandre868@gmail.com>
2026-03-20 16:43:57 +0100

    evenement modif

---
+++ html--php/evenement.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Evenements et Vie étudiante - Département Informatique EFREI</title>
+</head>
+<body>
+  <header>
+    <h1>Vie Étudiante &amp; Campus</h1>
+    <nav>
+      <ul>
+        <li><a href="accueil.html">Accueil</a></li>
+        <li><a href="cours.php">Cours et Formations</a></li>
+        <li><a href="equipe.php">Équipe d'enseignement</a></li>
+        <li><a href="evenement.php">Evenements et Vie étudiante</a></li>
+        <li><a href="apropos.html">À propos</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section>
+      <h2>Installations et moyens</h2>
+      <p>Le campus de Villejuif offre des laboratoires informatiques modernes, des espaces de coworking, des salles de projet et des ressources numériques accessibles aux étudiants.</p>
+      <p>Les étudiants ont également accès à des plateformes pédagogiques comme Moodle et des ressources et archives sur l'espcace Myefrei .</p>
+    </section>
+
+    <section>
+      <h2>Clubs et associations</h2>
+      <p>De nombreuses associations étudiantes sont liées à l’informatique :</p>
+    <ul>
+    <li>
+      <img src="images/asso1.png" alt="Logo association" width="50">
+      Associations
+    </li>
+
+    <li>
+      <img src="images/ice.png" alt="Logo ICE" width="50">
+      Associations d'innovations et conception : ICE
+    </li>
+
+    <li>
+      <img src="images/4esport.png" alt="Logo 4eSport" width="50">
+      Associations d’e-sport : 4eSport
+    </li>
+
+    <li>
+      <img src="images/cyber.png" alt="Logo cybersécurité" width="50">
+      Clubs de cybersécurité (hackathons, challenges, etc.)
+    </li>
+  </ul>
+    </section>
+
+    <section>
+      <h2>Témoignages</h2>
+      <p>Des étudiants actuels et des anciens (alumni) partagent leurs expériences, leurs projets et leurs parcours professionnels après l’EFREI.</p>
+      <p>Ces témoignages mettent en avant la richesse de la vie étudiante et la qualité de l’accompagnement pédagogique.</p>
+    </section>
+
+    <section>
+      <h2>Projets étudiants</h2>
+      <p>Une galerie dédiée présente les meilleurs projets réalisés dans le cadre des cours, des projets de groupe ou des associations.</p>
+    </section>
+  </main>
+
+  <footer>
+    <nav>
+      <a href="contact.php">À propos</a>
+    </nav>
+    <p>&copy; EFREI Paris - Département Informatique</p>
+  </footer>
+</body>
+</html>
