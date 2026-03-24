9ae7b3b58400acb06616b0028ca1cf40e9aec74a
Alexandre34420 <landryalexandre868@gmail.com>
2026-03-24 08:59:14 +0100

    modif img event

--- evenement.php
+++ evenement.php
@@ -23,14 +23,35 @@
       <h2>Installations et moyens</h2>
       <p>Le campus de Villejuif offre des laboratoires informatiques modernes, des espaces de coworking, des salles de projet et des ressources numériques accessibles aux étudiants.</p>
       <p>Les étudiants ont également accès à des plateformes pédagogiques comme Moodle et des ressources et archives sur l'espcace Myefrei .</p>
+    <ul>
+    <li>
+      <img src="https://tse2.mm.bing.net/th/id/OIP.g8hUyIQebqXKMeZvo8BYPQHaER?pid=Api&P=0&h=180" alt="Campus principal La Maison" width="120">
+      Campus principal de l'Efrei : La Maison 
+    </li>
+    
+    <li>
+      <img src="https://tse4.mm.bing.net/th/id/OIP.kgct9Pw-2GId8VWe5PWiHQHaE5?pid=Api&P=0&h=180"alt="laboratoires Innovations Labs" width="120">
+      Laboratoires Innovations Labs pour les projets 3D, maquettes et prototype accessibles aux étudiants
+    </li>
+
+    <li>
+      <img src="https://tse4.mm.bing.net/th/id/OIP.XE0oy6RXQ6kKRdCh6NvMkQHaEK?pid=Api&P=0&h=180" alt="Nouveau batiment New Republic" width="120">
+      Nouveau bâtiment depuis la rentrée 2026 : New Republic en face du campus principal La Maison
+    </li> 
+
+    <li>
+      <img src="https://tse2.mm.bing.net/th/id/OIP.uxdDDfupO05QdIMjigRX2wHaEK?pid=Api&P=0&h=180" alt="Salle de cours" width="120">
+      Salle de cours types à l'Efrei avec équipements pour les besoins des étudiants et enseignants
+    </li>
+    </ul>
     </section>
 
     <section>
       <h2>Découvrez tout les clubs et associations étudiantes</h2>
-      <p>De nombreuses associations étudiantes sont liés à différents domaines dans l'école comme Sport, tech, culture, humanitaire, médias… la vie associative de l’Efrei est riche et variée</p>
+      <p>De nombreuses associations étudiantes sont liés à différents domaines dans l'école comme le Sport, tech, culture, humanitaire, médias…, la vie associative de l’Efrei est riche et variée</p>
+      <p>Il existe plus de 65 associations et clubs</p>
     <ul>
     <li>
-    
       <img src="https://www.efrei.fr/wp-content/uploads/2025/08/One-Pantheon.png" alt="Logo association One Panthéon" width="50">
       Associations projets développement informatique : One Panthéon
     </li>
@@ -54,13 +75,14 @@
 
     <section>
       <h2>Témoignages pour découvrir le parcours des alumnis</h2>
-      <p>Des étudiants actuels et des anciens (alumni) partagent leurs expériences, projets et parcours professionnels après leurs expériences d'études l’EFREI.</p>
+      <p>Des anciens (alumni) partagent leurs expériences, projets et parcours professionnels après leurs expériences d'études l’EFREI.</p>
       <p>Ces témoignages mettent en avant la richesse de la vie étudiante et la qualité de l’accompagnement pédagogique.</p>
     </section>
 
     <section>
       <h2>Projets étudiants</h2>
       <p>Une galerie dédiée présente les meilleurs projets réalisés dans le cadre des cours, des projets de groupe ou des associations.</p>
+
     </section>
   </main>
 
